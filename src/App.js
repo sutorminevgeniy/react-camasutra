428eb182b6388d5f6fa304a570d80c0584e7c9c4
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2021-09-16 15:41:35 +0300

    lesson N7

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Simple HTML
+      <Header/>
+      <Technologies/>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Messages</a>
+    </div>
+  )
+};
+
+const Technologies = () => {
+  return (
+    <div>
       <ul>
         <li>css</li>
         <li>html</li>
@@ -12,7 +30,7 @@ function App() {
         <li>react</li>
       </ul>
     </div>
-  );
-}
+  )
+};
 
 export default App;
