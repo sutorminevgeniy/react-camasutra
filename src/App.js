b1808c9b4df3dd19380712a18b376ac719b889d0
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2021-09-16 16:34:13 +0300

    lesson N9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Header';
+import Technologies from './Technologies'
+
 function App() {
   return (
     <div className="App">
@@ -9,28 +12,5 @@ function App() {
     </div>
   );
 }
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#s">Home</a>
-      <a href="#s">News Feed</a>
-      <a href="#s">Messages</a>
-    </div>
-  )
-};
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  )
-};
 
 export default App;
