ac3061c088ceeba9f7fdb901fd456281d0e1a974
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2021-09-16 17:10:56 +0300

    lesson N10

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './Header';
 import Technologies from './Technologies'
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Technologies/>
+      <Footer/>
     </div>
   );
 }
