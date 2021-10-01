b5dc1077919732d66f34ad66d211af5d5d5448d5
Evgenii Sutormin <contr.evgenii.sutormin@pandadoc.com>
2021-10-01 13:04:07 +0300

    lesson N15

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
-function App() {
+const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <Profile />
-    </div>
-  );
+    </div>);
 }
 
 export default App;
